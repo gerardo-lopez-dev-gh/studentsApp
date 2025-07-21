d0527adb57193d0bedc8782001e553b8655f40b0
gerardo-lopez-dev-gh <gerardo.a.lopez1994@gmail.com>
2025-07-21 14:51:56 -0300

    Add student domain class
    Created the student.java file with fields, constructors, getters, setters, and toString method for managing student data in the domain layer.

---
+++ src/main/java/com/javalab/domain/student.java
@@ -0,0 +1,82 @@
+package com.javalab.domain;
+
+public class student {
+  private int id;
+  private String name;
+  private String lastName;
+  private String phoneNumber;
+  private String email;
+
+  public student() {}
+
+  public student(String name, String lastName, String phoneNumber, String email) {
+
+    this.name = name;
+    this.lastName = lastName;
+    this.phoneNumber = phoneNumber;
+    this.email = email;
+  }
+
+  public student(int id) {
+    this.id = id;
+  }
+
+  public student(int id, String name, String lastName, String phoneNumber, String email) {
+    this.id = id;
+    this.name = name;
+    this.lastName = lastName;
+    this.phoneNumber = phoneNumber;
+    this.email = email;
+  }
+
+  @Override
+  public String toString() {
+    return "student{" +
+            "id=" + id +
+            ", name='" + name + '\'' +
+            ", lastName='" + lastName + '\'' +
+            ", phoneNumber='" + phoneNumber + '\'' +
+            ", email='" + email + '\'' +
+            '}';
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getPhoneNumber() {
+    return phoneNumber;
+  }
+
+  public void setPhoneNumber(String phoneNumber) {
+    this.phoneNumber = phoneNumber;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+}
